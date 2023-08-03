ad76870539e6ce051c35afb446e369793829c153
Koonac <rick.plays@hotmail.com>
2023-08-02 22:27:09 -0300

    Projeto inicial aprendendo vuex

---
+++ src/components/Item.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<div class="row">
+			<div class="col-1" v-if="dados.id" v-text="dados.id" />
+			<div class="col" v-if="dados.nome" v-text="dados.nome" />
+			<div class="col-2" v-if="dados.turno" v-text="dados.turno" />
+			<div class="col-2" v-if="dados.escala" v-text="dados.escala" />
+			<div class="col-1">
+				<i class="bi-check2-square" style="cursor: pointer"></i>
+			</div>
+		</div>
+		<hr />
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Item",
+		props: {
+			dados: Object,
+		},
+	};
+</script>
---
+++ src/components/ListaItens.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="pt-3">
+		<item v-for="(item, index) in itens" :key="index" :dados="item" />
+	</div>
+</template>
+
+<script>
+	import Item from "@/components/Item.vue";
+	import { mapState } from "vuex";
+
+	export default {
+		name: "ListaItens",
+		components: {
+			Item,
+		},
+		props: {
+			tipo: String,
+		},
+		computed: {
+			...mapState({
+				enfermeiros: (state) => state.enfermeiros,
+				socorristas: (state) => state.socorristas,
+				medicos: (state) => state.medicos,
+			}),
+			itens() {
+				switch (this.tipo) {
+					case "enfermeiros":
+						return this.enfermeiros;
+					case "socorristas":
+						return this.socorristas;
+					case "medicos":
+						return this.medicos;
+				}
+				return [];
+			},
+		},
+	};
+</script>
